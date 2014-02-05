fa39a7238f2f4110bc95da32651f3a0fe77fc309
tri2sing <tri2sing@yahoo.com>
2014-02-04 23:20:14 -0800

    02/04/2014: Sameer Adhikari - Modified the code to take parameters, use defaults, and modify userdata

--- create_instance_userdata.py
+++ create_instance_userdata.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 import datetime
 import os
 import random
@@ -16,6 +17,9 @@ today = datetime.datetime.utcnow()
 today = today.replace (hour=0, minute=0, second=0, microsecond=0)
 
 VERSION = 2
+
+# As the credential information is sensiteive information, I assume it is set in environment variable 
+# on the host where this script runs and this script fails when the variables are not set.
 
 def get_kston_info():
     d = {}
@@ -47,54 +51,82 @@ def get_nova_info():
     d['no_cache'] = True
     return d
 
-random.seed()
 
-try:
-    nova = nc.Client (VERSION, **get_nova_info())
-    cinder = cct.Client (VERSION, **get_cinder_info())
-
-    # It is required that any of the search terms used in find functions
-    # match  objects that already exist in the OpenStack environment.
-    img = nova.images.find (name = 'Ubuntu_12.04')
-    #img = nova.images.find (name = 'WS08R2_ApplicationServer')
-    size = int(getattr(img, 'OS-EXT-IMG-SIZE:size'))
-    sizegb = size/(1024*1024*1000)
-    fvr = nova.flavors.find (name = 'Small-A')
-    net = nova.networks.find (label='StressTest')
-
-
-    # I am creating one instance at a time instead of the using the max_count parameter.
-    # Using max_count creates VMs appended with GUID in their names and makes them very long.
-
-    ranstr =  str(int(random.random()*1000))
-    #nme = 'sameer' + computer[-5:] + ranstr
-    nme = 'poctest01'
+def main(count=1, flavor='Large-B', image='WS08R2_ApplicationServer', zone='nova', host=os.uname()[1], network='External3', prefix='sameer', userdatafile=''):
     
-    vol1 = cinder.volumes.create(sizegb, imageRef=img.id)
-    status = vol1.status
-    while status != 'available':
-        time.sleep (5)
-        vol1 = cinder.volumes.get (vol1.id)
-        status = vol1.status
+    random.seed()
+    
+    try:
+        nova = nc.Client (VERSION, **get_nova_info())
+        cinder = cct.Client (VERSION, **get_cinder_info())
+    
+        # It is required that any of the search terms used in find functions
+        # match  objects that already exist in the OpenStack environment.
+        img = nova.images.find (name = image)
+        size = int(getattr(img, 'OS-EXT-IMG-SIZE:size'))
+        sizegb = size/(1024*1024*1000)
+        fvr = nova.flavors.find (name = flavor)
+        net = nova.networks.find (label=network)
+    
+    
+        # I am creating one instance at a time instead of the using the max_count parameter.
+        # Using max_count creates VMs appended with GUID in their names and makes them very long.
 
-    instance = nova.servers.create (
-        name=nme, 
-        image=img, 
-        flavor=fvr, 
-        nics=[{'net-id': net.id}], 
-        #availability_zone='nova:'+computer, 
-        block_device_mapping={'vda': vol1.id},
-        userdata=open('min-lnx').read()
+        for i in range(count):
+    
+            nme = prefix + str(int(random.random()*1000))
+            
+            vol1 = cinder.volumes.create(sizegb, imageRef=img.id)
+            status = vol1.status
+            while status != 'available':
+                time.sleep (5)
+                vol1 = cinder.volumes.get (vol1.id)
+                status = vol1.status
+        
+            if userdatafile:
+                userdata=open(userdatafile).read().replace('hostnamereplace', nme)
+            else:
+                userdata = ''
+                
+            instance = nova.servers.create (
+                name=nme, 
+                image=img, 
+                flavor=fvr, 
+                nics=[{'net-id': net.id}], 
+                availability_zone=zone+':'+computer, 
+                block_device_mapping={'vda': vol1.id},
+                userdata=userdata
+                )
+        
+            status = instance.status
+            while status == 'BUILD':
+                time.sleep(5)
+                # Retrieve instance again to update status field
+                instance = nova.servers.get (instance.id)
+                status = instance.status
+
+    except nexc.ClientException as exc:
+        print vars(exc)
+        sys.exit()
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('-c', '--count', type=int, default=1, help='the number of instances to create') 
+    parser.add_argument ('-f', '--flavor', default='Large-B', help='name of the flavor to use for the instance')
+    parser.add_argument ('-i', '--image', default='WS08R2_ApplicationServer', help='name of the image to use for the instance')
+    parser.add_argument ('-az', '--zone', default='nova', help='availability zone to launch the instance on')
+    parser.add_argument ('-t', '--host', default=os.uname()[1], help='name of the host to launch the instance on')
+    parser.add_argument ('-n', '--network', default='External3', help='name of the network to use for the instance')
+    parser.add_argument ('-p', '--prefix', default='sameer', help='prefix to use for the name of the instance, the remainder will be a random number')
+    parser.add_argument ('-u', '--userdatafile', default='', help='path to file to run at the instance on intial boot, usually for customization')
+    args = parser.parse_args()
+    main(
+        count=args.count, 
+        flavor=args.flavor, 
+        image=args.image, 
+        zone=args.zone, 
+        host=args.host, 
+        network=args.network, 
+        prefix=args.prefix, 
+        userdatafile=args.userdatafile
         )
-
-    status = instance.status
-    while status == 'BUILD':
-        time.sleep(5)
-        # Retrieve instance again to update status field
-        instance = nova.servers.get (instance.id)
-        status = instance.status
-except nexc.ClientException as exc:
-    print vars(exc)
-    sys.exit()
-
-
